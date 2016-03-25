d5f9b9626eea9e7ddd2dc4b514a36fa2b3c9a25a
Thomas Lo <thomasjinlo@gmail.com>
2016-03-25 09:31:47 -0700

    angular git add -A!

--- js/controllers/todoController.js
+++ js/controllers/todoController.js
@@ -1,7 +1,14 @@
 todoApp.controller('TodoCtrl', [
-  '$scope', function($scope){
+  '$scope', '$window', function($scope, $window){
     $scope.items = [{ text: "Get groceries from the store",
                 dueDate: new Date(),
                 completed: false }];
+
+    $scope.createTodo = function(text, date) {
+      var todoObj = {text: text, dueDate: date, completed: false}
+      $scope.text = '';
+      $scope.date = '';
+      $scope.items.push(todoObj);
+    }
   }
 ]);
